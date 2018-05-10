a5a50c8b9cdb75f22c6c7b8a5f16aeed362592c6
Jesus Martinez <34748709+accesus@users.noreply.github.com>
2018-05-10 16:06:00 -0600

    Update matryushka.js

--- matryushka.js
+++ matryushka.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	html = document.documentElement.outerHTML;
 	cs = checksum(html);
 	console.log(cs);
-	if (cs=="13b6d088") {
+	if (cs=="37fc4a7d") {
 	} else {
 		$("body").html("").append("<div style='width:200px;text-align:center;background:red;border:2px solid #ab0000;border-radius:10px;color:#ffffff;padding:10px;'><strong style='font-size:31px;'>ALERT!</strong><br>This code is a copy!<br><hr>Visit <a href='https://www.sololearn.com/Profile/8355093'><b>VITRUVIAN</b></a> profile<br>to see the original.</div>");
 	}
